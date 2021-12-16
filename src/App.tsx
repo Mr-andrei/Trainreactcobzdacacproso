64847230524db138b5e3a220eb0aea99e9ed2103
Mr-andrei <91520213+Mr-andrei@users.noreply.github.com>
2021-12-16 23:01:49 +0300

    next commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,20 @@
 import React, {useState} from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 import {MyPush} from './components/OnOff/OnOff'
 import {Accordion} from "./components/accordion";
-import {Rating} from "./components/Rating";
+
 import {NewRating, ratingValueTupe} from "./components/NewRating";
 import {NewAccordion} from "./components/Newaccordion";
 import {Input, InputRef} from "./components/input";
-import {Select} from "./components/Select";
+import Select from "./components/Select";
 
 
-export type oneCity = {
-    id: number
-    title: string
-}
-type typeOfCountry = Array<oneCity>
+
 
 
 function App() {
 
-
-
-
-    const [myCountry, setmyCountry] = useState<Array<oneCity>>([
-        {id: 2, title: "Minsk"},
-        {id: 2, title: "Gomel"},
-        {id: 2, title: "Mogelev"},
-        {id: 1, title: "Kiev"},
-        {id: 1, title: "Lviv"},
-        {id: 1, title: "Odesa"},
-        {id: 3, title: "Moscva"},
-        {id: 3, title: "Piter"},
-        {id: 3, title: "Tymen"},
-    ])
-
-    const [valueCountry, setValueCountry] = useState(false)
-
-   const openCountruList =( ) => {
-        setValueCountry(!valueCountry)
-    }
-    /*
-    const addContry = (title:string) => {
-        let newCountry = {id : 6, title: title}
-        setmyCountry([newCountry, ...myCountry])
-
-    }*/
 
 
     const [star, setStart] = useState<ratingValueTupe>(4)
@@ -60,9 +30,7 @@ function App() {
             <Input/>
             <InputRef/>
             <Select
-                myData={myCountry}
-                valueCountry={valueCountry}
-                openCountruList={openCountruList}
+
             />
         </div>
     );
--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,30 +1,82 @@
-import {oneCity} from "../App";
-import {ChangeEvent, useState} from "react";
+
+import {useState} from "react";
 
 
-type selectPropsType = {
-    myData: Array<oneCity>
-    openCountruList: () => void
-    valueCountry:boolean
+// type poropstype ={
+//     newCountry:Array<oneCity>
+//     countryFilter: (type:string)=> void
+// }
 
+
+export type oneCity = {
+    id: number
+    title: string
 }
 
-export const Select = ({myData, openCountruList, valueCountry}: selectPropsType) => {
 
 
 
 
 
+const Select = ( ) => {
+
+    const [myCountry, setmyCountry] = useState<Array<oneCity>>([
+        {id: 2, title: "Minsk"},
+        {id: 2, title: "Gomel"},
+        {id: 2, title: "Mogelev"},
+        {id: 1, title: "Kiev"},
+        {id: 1, title: "Lviv"},
+        {id: 1, title: "Odesa"},
+        {id: 3, title: "Moscva"},
+        {id: 3, title: "Piter"},
+        {id: 3, title: "Tymen"},
+    ])
+
+    const [count, setCount] = useState<number>(0)
+
+    const [countryFilter, setCountryFilter] = useState("")
+    //
+    let newCountry = myCountry
+    if (countryFilter === "bel") {
+        newCountry = myCountry.filter(f => f.id === 2)
+
+    }
+    if (countryFilter === "uk") {
+        newCountry = myCountry.filter(f => f.id === 1)
+    }
+    if (countryFilter === "russ") {
+        newCountry = myCountry.filter(f => f.id === 3)
+    }
+    if (countryFilter === "all") {
+        newCountry = myCountry.filter(f => f)
+    }
 
 
+  let  countryOfFilter = newCountry.map((m,i) => <div key={i}>{m.title}</div>)
 
 
-    let Data = myData.map(f => {
-        return( f.id === 2 ? <div>{f.title}</div> : "")})
-    return (
-        <div>
-            {Data}
-        </div>
+    const countPlus =()=> {
+        setCount(count+1)
+        console.log("count+")
+    }
+    const filterBel =()=> {
+        setCountryFilter("bel")
+        console.log("filter")
+    }
+
+    return(
+        <div>{countryOfFilter}
+        <button onClick={filterBel} > bel </button>
+        <button onClick={()=> setCountryFilter("uk")} > uk </button>
+        <button onClick={()=> setCountryFilter("russ")} > russ </button>
+        <button onClick={()=> setCountryFilter("all")} > all </button>
+      <div>
+            <span>{count}</span>
+            <button onClick={countPlus} >+</button>
+      </div>
+      </div>
 
     )
 }
+
+export default Select
